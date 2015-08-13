85643df5f748a5963ad5201a3705e1ce380e2d40
VladimirShe <Snayki_007_Sheva>
2015-08-13 22:25:13 +0300

    update Dropdown rendering

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-function Dropdown(level, containerSelector, callback) {
-    this.level = level;
-    this.callback = callback;
-    this._init(containerSelector);
+var Dropdown = function() {
+    var _level,
+        _callback,
+        _oContainer,
+        _items,
+        _oSelect;
 
-    this.getItemById = function (itemId) {
-        for (var i = 0; i < this.items.length; i++) {
-            if (this.items[i]['id'] === itemId) {
-                return this.items[i];
+    function Dropdown(level, oContainer, callback) {
+        _level = level;
+        _oContainer = oContainer;
+        _callback = callback;
+        _items = [];
+
+        _init();
+        //Dropdown.prototype.render([]);
+    }
+
+    var _getItemById = function (itemId) {
+        for (var i = 0; i < _items.length; i++) {
+            if (_items[i]['id'] == itemId) {
+                return _items[i];
             }
         }
     };
-}
 
+    var _init = function () {
+        _oSelect = document.createElement("select");
+        _oSelect.id = "dropdown-" + _level;
+        _oSelect.options.add( new Option("Not Selected", -1) );
+    };
 
-Dropdown.prototype._init = function (containerSelector) {
-    this.items = [];
-    this.container = document.querySelector(containerSelector);
+    Dropdown.prototype.clear = function () {
+        _items = [];
+        _oSelect.disabled = false;
 
-    this.select = document.createElement("select");
-    this.select.id = "dropdown-" + this.level;
-    this.select.options.add( new Option(-1, "Not Selected") );
-};
-
-Dropdown.prototype.render = function (items) {
-    var self = this;
-    this.clear();
-    this.items = items;
-
-    if (items.length > 0) {
-        for (var i = 0; i < items.length; i++) {
-            this.select.options.add( new Option(items[i]['id'], items[i]['name']) );
+        for (var i = 1; i <= _oSelect.options.length; i++) {
+            _oSelect.options[i] = null;
         }
-    } else {
-        this.select.disabled = true;
-    }
+    };
 
-    var frag = document.createDocumentFragment();
+    Dropdown.prototype.render = function (items) {
+        this.clear();
+        _items = items;
 
-    this.select.addEventListener("change", function () {
-        var itemId = this.options[this.selectedIndex].value;
-        var item = self.getItemById(itemId);
-        self.callback(self.level, item);
-    });
-    frag.appendChild(this.select);
-    this.container.appendChild(frag);
-};
+        if (_items.length > 0) {
+            for (var i = 0; i < _items.length; i++) {
+                _oSelect.options.add( new Option(_items[i]['name'], _items[i]['id']) );
+            }
+        } else {
+            _oSelect.disabled = true;
+        }
 
-Dropdown.prototype.clear = function () {
-    this.items = [];
-    this.select.disabled = false;
+        var frag = document.createDocumentFragment();
 
-    for (var i = 1; i <= this.select.options.length; i++) {
-        this.select.options[i] = null;
-    }
-};
+        _oSelect.addEventListener("change", function () {
+            var itemId = this.options[this.selectedIndex].value;
+            var item = _getItemById(itemId);
+            _callback(_level, item);
+        });
+        frag.appendChild(_oSelect);
+        _oContainer.appendChild(frag);
+    };
+
+    return Dropdown;
+}();
+
+var d = new Dropdown(0, document.querySelector('#app-content'), function (level, item) {console.log(level, item);});
+d.render([{id:1, name: 'sdfsdfsdf'}, {id:2, name: 'fsdfsdfsdfsdfsdf'}]);
+console.log(d);
