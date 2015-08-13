c180457c94f1bffdbe44f8353e0ad5d415ea74e6
Shevchenko Alexander <loki.sup@gmail.com>
2015-08-13 23:38:43 +0300

    extract test data

---
+++ data.js
@@ -0,0 +1,23 @@
+var testData = [
+    {id: 1, name: 'A', parentId: null},
+    {id: 2, name: 'B', parentId: null},
+    {id: 3, name: 'C', parentId: null},
+    {id: 4, name: '2', parentId: 1},
+    {id: 5, name: '4', parentId: 1},
+    {id: 6, name: '6', parentId: 1},
+    {id: 7, name: '1', parentId: 2},
+    {id: 8, name: '3', parentId: 2},
+    {id: 9, name: '10', parentId: 2},
+    {id: 10, name: '50', parentId: 3},
+    {id: 11, name: '60', parentId: 3},
+    {id: 12, name: '70', parentId: 3},
+    {id: 13, name: '70', parentId: 9},
+    {id: 14, name: 'PI', parentId: 9},
+    {id: 15, name: 'Segma', parentId: 9},
+    {id: 16, name: 'Alpha', parentId: 11},
+    {id: 17, name: '3', parentId: 11},
+    {id: 18, name: 'Zeta', parentId: 11},
+    {id: 19, name: 'Test', parentId: 14},
+    {id: 20, name: 'Foo', parentId: 14},
+    {id: 21, name: 'Bar', parentId: 14}
+];
